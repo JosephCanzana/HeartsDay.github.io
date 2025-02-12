6d6aa8b049cfe21612edbe40f393f99149cf7411
JosephCanzana <josephcanzana8@gmail.com>
2025-02-12 10:40:32 +0800

    Baseline Idea of the Website

---
+++ script.js
@@ -0,0 +1,108 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const backgroundMusic = document.getElementById('backgroundMusic');
+    backgroundMusic.muted = true; // Mute initially to allow autoplay
+    setTimeout(() => {
+        backgroundMusic.play().then(() => {
+            backgroundMusic.muted = false; // Unmute after autoplay starts
+        }).catch(error => {
+            console.log('Autoplay was prevented:', error);
+            // Optionally, you can add a button to start the music manually
+        });
+    }, 1000); // 1-second delay
+});
+
+document.getElementById('randomButton').addEventListener('click', function() {
+    const button = this;
+    const body = document.body;
+    const gif = document.querySelector('.gif img');
+    const gifContainer = document.querySelector('.gif');
+    const questionContainer = document.querySelector('.question-container');
+    const yesButton = document.getElementById('yesButton');
+    const videoContainer = document.querySelector('.video-container');
+    const noVideo = document.getElementById('noVideo');
+    const buttonContainer = document.querySelector('.button-container');
+
+    const maxX = body.clientWidth - button.offsetWidth;
+    const maxY = body.clientHeight - button.offsetHeight;
+
+    let randomX, randomY;
+
+    // Function to check if the button overlaps with an element
+    function isOverlapping(element) {
+        const buttonRect = button.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
+        return !(buttonRect.right < elementRect.left ||
+                 buttonRect.left > elementRect.right ||
+                 buttonRect.bottom < elementRect.top ||
+                 buttonRect.top > elementRect.bottom);
+    }
+
+    do {
+        randomX = Math.floor(Math.random() * maxX);
+        randomY = Math.floor(Math.random() * maxY);
+        button.style.position = 'absolute';
+        button.style.left = randomX + 'px';
+        button.style.top = randomY + 'px';
+    } while (isOverlapping(gif) || isOverlapping(questionContainer) || isOverlapping(yesButton));
+
+    // Array of "No" messages
+    const noMessages = [
+        "Are you sure? 😢", "Think again! 🤔", "Don't be so cold! 🥶",
+        "Maybe just a little? 🥺", "Why not? 🤨", "Give it another thought! 🤯",
+        "Just one yes? 🥹","Don't be that person... 😒", "I'm running out of ways to convince you! 😵",
+        "Fine, but you're missing out! 👀", "Okay, I give up! 😩"
+    ];
+
+    // Counter to limit message changes
+    if (typeof window.counter === 'undefined') {
+        window.counter = 0;
+    }
+
+    if (window.counter < noMessages.length) {
+        button.innerText = noMessages[window.counter]; // Change button text
+        window.counter++;
+    } else {
+        // Hide the GIF, show the video, and update the question text
+        gifContainer.style.display = 'none';
+        videoContainer.style.display = 'block';
+        questionContainer.innerText = "😜 I will 'Never gonna give you up 🤌'";
+        buttonContainer.style.display = 'none';
+        noVideo.play();
+    }
+
+    // Increase the size of the "Yes" button
+    const currentWidth = yesButton.offsetWidth;
+    const currentHeight = yesButton.offsetHeight;
+    yesButton.style.width = (currentWidth + 10) + 'px';
+    yesButton.style.height = (currentHeight + 10) + 'px';
+
+    // Array of GIF file paths
+    const gifPaths = [
+        "images/thinkAgain.gif", "images/flowerSquirtle.gif", "images/Snow.gif",
+        "images/squirtleLittle.gif", "images/twoSquirtle.gif", "images/eatingSquirtle.gif",
+        "images/youSure.gif", "images/StumpingSquirtle.gif", "images/runningCrying.gif",
+        "images/SquirtleHeavyCry.gif", "images/layingCrying.gif"
+    ];
+
+    // Counter to keep track of the current GIF index
+    if (typeof window.gifCounter === 'undefined') {
+        window.gifCounter = 0;
+    }
+
+    // Change the GIF in order
+    gif.src = gifPaths[window.gifCounter];
+    window.gifCounter = (window.gifCounter + 1) % gifPaths.length; // Loop back to the start if at the end
+});
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    const questionContainer = document.getElementById('questionContainer');
+    const gif = document.querySelector('.gif img');
+    const buttonContainer = document.querySelector('.button-container');
+
+    // Set the text and GIF for the "Yes" response
+    questionContainer.innerText = "Thank you for saying Yes! 🎉";
+    gif.src = "images/happySquirtle.gif"; // Change this to the path of your "Yes" GIF
+
+    // Hide the buttons
+    buttonContainer.style.display = 'none';
+});
